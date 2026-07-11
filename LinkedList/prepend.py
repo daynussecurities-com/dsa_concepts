c6943a0afc1671a990fdd36e61ef5a8e5c2db3c6
pandiyarajanbt <pandiyarajaniosde@gmail.com>
2026-07-11 21:24:01 +0530

    added new files prepended

---
+++ LinkedList/prepend.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_items(self):
+        val = self.head
+        while val is not None:
+            print(val.value)
+            val = val.next
+
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        pre = self.head
+        temp = self.head
+        while (temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        return True
+
+obj = LinkedList(3)
+obj.append(2)
+obj.append(6)
+obj.pop()
+obj.print_items()
